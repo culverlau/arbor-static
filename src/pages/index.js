9f687ea98f5f9dfa3e1a83acdb058e767b1afc73
Culver Lau <CulverL@symmetryelectronics.com>
2018-12-27 16:55:55 -0800

    added hero section, fixed breakpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,64 +3,43 @@ import React from 'react'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
 
+import { siteMetadata } from '../../gatsby-config'
 import Post from 'templates/Post'
 import Meta from 'components/Meta'
 import Layout from 'components/Layout'
 
-const BlogIndex = ({ data, location }) => {
-  const posts = get(data, 'remark.posts')
-  return (
-    <Layout location={location}>
-      <Meta site={get(data, 'site.meta')} />
-      {posts.map(({ post }, i) => (
-        <Post
-          data={post}
-          options={{
-            isIndex: true,
-          }}
-          key={i}
-        />
-      ))}
-    </Layout>
-  )
+class Home extends React.Component {
+  render() {
+    const { location, data } = this.props
+    const hero = get(data, 'hero.childImageSharp.fluid')
+
+    return (
+      <Layout location={location}>
+        <Meta site={siteMetadata} title="Home" />
+        <section className="hero-section">
+          <figure>
+            <Img fluid={hero} alt="" />
+          </figure>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-12">
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
 }
 
-export default BlogIndex
+export default Home
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      meta: siteMetadata {
-        title
-        description
-        url: siteUrl
-        author
-        twitter
-        adsense
-      }
-    }
-    remark: allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      posts: edges {
-        post: node {
-          html
-          frontmatter {
-            layout
-            title
-            path
-            category
-            tags
-            description
-            date(formatString: "YYYY/MM/DD")
-            image {
-              childImageSharp {
-                fixed(width: 500) {
-                  ...GatsbyImageSharpFixed_withWebp
-                }
-              }
-            }
-          }
+  query {
+    hero: file(name: { eq: "home-garden" }) {
+      childImageSharp {
+        fluid(maxWidth: 2048) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
